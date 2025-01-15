3f4e8b5e7a866ae26057430955b2369980fd44cf
Angela Hu <angelahu925@gmail.com>
2025-01-14 22:34:19 -0800

    Added image activity

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,11 +1,20 @@
 import ThumbsUp from "../../../public/assets/thumbs-up.svg";
 import ThumbsDown from "../../../public/assets/thumbs-down.svg";
+import Dogs from "../../../public/assets/dogs.jpg";
 import Image from "next/image";
 import styles from "./style.module.css";
 
 const Post = ({ user, content, likes, dislikes }) => {
   return (
     <div className={styles.container}>
+      {/* Uncomment this during Activity #4 */}
+      {/* <Image
+        className={styles.dogImage}
+        src={Dogs}
+        width={500}
+        height={300}
+        alt="dogs"
+      /> */}
       <div className={styles.userInteractions}>
         <h3>{user}</h3>
         <div className={styles.likesContainer}>
